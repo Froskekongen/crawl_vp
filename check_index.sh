ad24e2926f8aab17f24affed77e64e92db9e68c0
Froskekongen <erlend.aune.1983@gmail.com>
2014-07-22 13:45:04 +0200

    .

---
+++ check_index.sh
@@ -0,0 +1,21 @@
+curl -XPUT 'http://172.30.31.203:9200/wines/product/89101' -d '{
+    "Prodnum" : 89101,
+    "Producer" : "Lanson Pére et Fils",
+    "Price" : 369
+}'
+
+sleep 2s
+#curl -XGET 'http://172.30.31.203:9200/wines/_mapping/product'
+
+curl -XGET 'http://172.30.31.203:9200/wines/_search' -d '{
+    "query": {
+        "filtered": {
+            "filter": {
+                "term": { "Prodnum": 89101 }
+            }
+        }
+    }
+}'
+
+
+curl -XDELETE 'http://172.30.31.203:9200/wines/product/89101'
